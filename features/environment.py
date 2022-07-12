e8a57a57d503381de2a1ff308ce6b8a408ed4fb5
Roman Skliarov <skliarovrn@Romans-Air.fios-router.home>
2022-07-12 10:59:27 -0400

    update

--- features/environment.py
+++ features/environment.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.events import EventFiringWebDriver
 
-# from support.logger import logger, MyListener
+from support.logger import logger, MyListener
 
 # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
 bs_user = 'romanskliarov_x8pMVg'
@@ -21,30 +21,30 @@ def browser_init(context, test_name):
     :param context: Behave context
     :param test_name: scenario.name
     """
-    # context.driver = webdriver.Chrome(executable_path='/Users/skliarovrn/Documents/GetTop/chromedriver')
+    # context.driver = webdriver.Chrome(executable_path='/Users/skliarovrn/Desktop/automationRoman/python-selenium-automation/chromedriver')
     # context.driver = webdriver.Firefox(executable_path='')
     # context.driver = webdriver.Safari()
 
     # HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
     # options.add_argument('--headless')
-    # context.driver = webdriver.Chrome(chrome_options=options, executable_path='/Users/skliarovrn/Documents/GetTop/chromedriver')
+    # context.driver = webdriver.Chrome(chrome_options=options, executable_path='/Users/skliarovrn/Desktop/automationRoman/python-selenium-automation/chromedriver')
 
     ### EventFiringWebDriver - log file ###
     ### for drivers ###
-    # context.driver = EventFiringWebDriver(webdriver.Chrome(executable_path='/Users/skliarovrn/Documents/GetTop/chromedriver'), MyListener())
+    context.driver = EventFiringWebDriver(webdriver.Chrome(executable_path='/Users/skliarovrn/Desktop/automationRoman/python-selenium-automation/chromedriver'), MyListener())
     # for headless mode ###
     # context.driver = EventFiringWebDriver(webdriver.Chrome(chrome_options = options), MyListener())
 
     ### for browerstack ###
-    desired_cap = {
-        'browser': 'Chrome',
-        'os_version': 'Monterey',
-        'os': 'OS X',
-        'name': test_name
-    }
-    url = f'http://{bs_user}:{bs_pw}@hub-cloud.browserstack.com/wd/hub'
-    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+    # desired_cap = {
+    #     'browser': 'Chrome',
+    #     'os_version': 'Monterey',
+    #     'os': 'OS X',
+    #     'name': test_name
+    # }
+    # url = f'http://{bs_user}:{bs_pw}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     # context.driver.maximize_window()
     context.driver.implicitly_wait(5)
@@ -55,7 +55,7 @@ def browser_init(context, test_name):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    # logger.info(f'Started scenario: {scenario.name}')
+    logger.info(f'Started scenario: {scenario.name}')
     browser_init(context, scenario.name)
 
 
@@ -66,7 +66,7 @@ def before_step(context, step):
 
 def after_step(context, step):
     if step.status == 'failed':
-        # logger.error(f'Step failed: {step}')
+        logger.error(f'Step failed: {step}')
         print('\nStep failed: ', step)
         # Mark test case as FAILED on BrowserStack:
         context.driver.execute_script(
